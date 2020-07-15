c42ec8563381e7f69b5ceb099c73dea603b416e0
Rodrigo de Almeida Alves <rodrigoaaeng@gmail.com>
2020-07-14 21:08:06 -0300

    Estutos - Commite para testes Github - App com novas funcionalidades de agenda

---
+++ app/src/main/java/com/example/rodri/primeirospassos/FormularioActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rodri.primeirospassos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FormularioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.formulario)
+    }
+}
